61140761cdf68cda97cbf73f8f50d252b4274808
unknown <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-01-21 21:12:33 -0300

    agregue un componente card

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -5,9 +6,26 @@ class CardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Card Widget"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        children: [
+          Card(
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.album_outlined, color: AppTheme.primary),
+                  title: const Text("Titulo"),
+                  subtitle: const Text(
+                      "Non exercitation tempor ipsum fugiat incididunt commodo ex tempor consectetur quis."),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
