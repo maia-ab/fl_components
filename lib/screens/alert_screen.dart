628cae48e6cb658502cb13459328a815528b6c9a
unknown <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-01-23 00:10:23 -0300

    agregue alerta y estilo de alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -4,22 +4,47 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            title: const Text("Titulo"),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusDirectional.circular(10)),
+            content: const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Este es el contenido de la alerta"),
+                SizedBox(height: 10),
+                FlutterLogo(size: 50)
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text("Cancelar"))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
+            onPressed: () => displayDialog(context),
             child: const Padding(
               padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
               child: Text("Mostrar Alerta"),
-            ),
-            onPressed: () {}),
+            )),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.close),
-        onPressed: () {
-          Navigator.pop(context);
-        },
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
