35906942c045ae9966f4b8282f96d70920fa6a9d
unknown <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-01-22 23:44:18 -0300

    edite screen de alertas y edite los estilos de los botones

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,14 +1,25 @@
+import 'package:fl_components/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
-   
   const AlertScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('AlertScreen'),
+        child: ElevatedButton(
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+              child: Text("Mostrar Alerta"),
+            ),
+            onPressed: () {}),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        onPressed: () {
+          Navigator.pop(context);
+        },
       ),
     );
   }
--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -4,9 +4,25 @@ class AppTheme {
   static Color? primary = Colors.lightGreen;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      //Color primario
-      primaryColor: Colors.green,
-      appBarTheme: AppBarTheme(color: primary, elevation: 0),
-      textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(foregroundColor: AppTheme.primary)));
+    //Color primario
+    primaryColor: Colors.green,
+    appBarTheme: AppBarTheme(color: primary, elevation: 0),
+
+    textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(foregroundColor: AppTheme.primary)),
+
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: AppTheme.primary,
+      elevation: 5,
+    ),
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: AppTheme.primary,
+          shape: const StadiumBorder(),
+          elevation: 0,
+          textStyle: const TextStyle(fontSize: 20)),
+    ),
+  );
 }
